1e14fa2730c19097a8dce1c5db560805b7eb5d14
xor <xor@freenetproject.org>
2008-10-28 00:22:51 +0000

    New class.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/FMSPlugin@23129 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ WoT/FMSMessageManagerWoT.java
@@ -0,0 +1,26 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
+package plugins.FMSPlugin.WoT;
+
+import java.util.Iterator;
+
+import plugins.FMSPlugin.FMSBoard;
+import plugins.FMSPlugin.FMSMessageManager;
+import plugins.FMSPlugin.FMSOwnIdentity;
+
+public class FMSMessageManagerWoT extends FMSMessageManager {
+
+	@Override
+	public FMSBoard getBoardByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<FMSBoard> iterator(FMSOwnIdentity identity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
