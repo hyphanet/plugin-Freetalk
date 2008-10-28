b99254290829d5ce7d6a4c408824c31d2c2a0889
xor <xor@freenetproject.org>
2008-10-28 00:12:18 +0000

    Make the FMSBoard and FMSMessage iterators require an FMSOwnIdentity as parameter. I suppose we will make the FMSMessageManager store the union of the (trust-based) message sets from all own identities and make the iterators select the view of the passed identity from that union-set.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/FMSPlugin@23128 67a373e5-eb02-0410-a15c-ee090a768436

--- src/plugins/FMSPlugin/FMSBoard.java
+++ src/plugins/FMSPlugin/FMSBoard.java
@@ -9,7 +9,7 @@ import java.util.Iterator;
  * @author xor
  *
  */
-public class FMSBoard implements Iterable<FMSMessage> {
+public class FMSBoard {
 
 	private final String mName;
 	private String mDescription;
@@ -40,7 +40,7 @@ public class FMSBoard implements Iterable<FMSMessage> {
 		return mName;
 	}
 
-	public Iterator<FMSMessage> iterator() {
+	public Iterator<FMSMessage> iterator(FMSOwnIdentity identity) {
 		// TODO Auto-generated method stub
 		return null;
 	}
