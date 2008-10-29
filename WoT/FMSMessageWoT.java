c90aa69fba6a52dd618d943eec90fd1ad7d7c1ed
xor <xor@freenetproject.org>
2008-10-29 14:16:44 +0000

    Implementation skeleton.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/FMSPlugin@23169 67a373e5-eb02-0410-a15c-ee090a768436

--- WoT/FMSMessageWoT.java
+++ WoT/FMSMessageWoT.java
@@ -5,8 +5,10 @@ package plugins.FMSPlugin.WoT;
 
 import java.util.Date;
 import java.util.List;
+import java.util.SortedSet;
 
 import freenet.keys.FreenetURI;
+import plugins.FMSPlugin.FMSBoard;
 import plugins.FMSPlugin.FMSIdentity;
 import plugins.FMSPlugin.FMSMessage;
 
@@ -16,33 +18,23 @@ import plugins.FMSPlugin.FMSMessage;
  */
 public class FMSMessageWoT extends FMSMessage {
 
-	/**
-	 * @param newURI
-	 * @param newAuthor
-	 * @param newTitle
-	 * @param newDate
-	 */
-	public FMSMessageWoT(FreenetURI newURI, FMSIdentity newAuthor, String newTitle, Date newDate) {
-		super(newURI, newAuthor, newTitle, newDate);
+
+	public FMSMessageWoT(FreenetURI newURI, SortedSet<FMSBoard> newBoards, FMSIdentity newAuthor, String newTitle, Date newDate) {
+		super(newURI, newBoards, newAuthor, newTitle, newDate);
 		// TODO Auto-generated constructor stub
 	}
 
-	/* (non-Javadoc)
-	 * @see plugins.FMSPlugin.FMSMessage#getAttachments()
-	 */
 	@Override
 	public List<FreenetURI> getAttachments() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	/* (non-Javadoc)
-	 * @see plugins.FMSPlugin.FMSMessage#getText()
-	 */
 	@Override
 	public String getText() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 
 }
