b9b4521da201d0d212c317e2fa3eb0270cfec052
xor <xor@freenetproject.org>
2008-10-29 14:16:44 +0000

    Implementation skeleton.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/FMSPlugin@23169 67a373e5-eb02-0410-a15c-ee090a768436

--- src/plugins/FMSPlugin/WoT/FMSOwnIdentityWoT.java
+++ src/plugins/FMSPlugin/WoT/FMSOwnIdentityWoT.java
@@ -4,8 +4,13 @@
 package plugins.FMSPlugin.WoT;
 
 import java.util.Date;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 import freenet.keys.FreenetURI;
+import plugins.FMSPlugin.FMSBoard;
+import plugins.FMSPlugin.FMSIdentity;
+import plugins.FMSPlugin.FMSMessage;
 import plugins.FMSPlugin.FMSOwnIdentity;
 import plugins.WoT.Identity;
 import plugins.WoT.OwnIdentity;
@@ -15,6 +20,8 @@ import plugins.WoT.OwnIdentity;
  *
  */
 public class FMSOwnIdentityWoT extends FMSIdentityWoT implements FMSOwnIdentity {
+	
+	private final LinkedList<FMSBoard> mSubscribedBoards = new LinkedList<FMSBoard>();
 
 	public FMSOwnIdentityWoT(OwnIdentity newIndentity) {
 		super(newIndentity);
@@ -28,7 +35,52 @@ public class FMSOwnIdentityWoT extends FMSIdentityWoT implements FMSOwnIdentity 
 		return getOwnIdentity().getInsertURI();
 	}
 
-	public Date getLastInsert() {
+	public synchronized Date getLastInsert() {
 		return getOwnIdentity().getLastInsert();
 	}
+
+	public synchronized void postMessage(FMSMessage message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public synchronized void subscribeToBoard(FMSBoard board) {
+		if(mSubscribedBoards.contains(board)) {
+			assert(false); /* TODO: Add logging / check whether this should be allowed to happen */
+			return;
+		}
+		mSubscribedBoards.add(board);
+	}
+
+	public synchronized void unsubscribeFromBoard(FMSBoard board) {
+		mSubscribedBoards.remove(board);
+	}
+	
+	public synchronized Iterator<FMSBoard> subscribedBoardsIterator() {
+		return mSubscribedBoards.iterator();
+	}
+
+	public synchronized boolean wantsMessagesFrom(FMSIdentity identity) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	/*
+	public final void exportXML(OutputStream out) throws IOException {
+		Writer w = new BufferedWriter(new OutputStreamWriter(out));
+		w.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+		w.write("<Identity\n");
+		w.write("\t<Name><![CDATA[");
+		XMLUtils.writeEsc(w, getNickName());
+		w.write("]]></Name>\n");
+		
+		w.write("\t<SingleUse>false</SingleUse>\n");
+		w.write("\t<PublishTrustList>false</PublishTrustList>\n");
+		w.write("\t<PublishBoardList>false</PublishBoardList>\n");
+
+		w.write("<Identity\n");
+		w.flush();
+		w.close();
+	}
+	*/
 }
