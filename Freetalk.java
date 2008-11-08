80c25e1ae7181fa7f5593800d8678ee1fe4823e3
xor <xor@freenetproject.org>
2008-11-08 10:23:26 +0000

    Fix webinterface bug.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/Freetalk@23413 67a373e5-eb02-0410-a15c-ee090a768436

--- Freetalk.java
+++ Freetalk.java
@@ -171,8 +171,10 @@ public class Freetalk implements FredPlugin, FredPluginFCP, FredPluginHTTP, Fred
 	public String handleHTTPGet(HTTPRequest request) throws PluginHTTPException {
 
 		String pass = request.getParam("formPassword");
-		if (pass == null || (pass.length() == 0) || !pass.equals(pr.getNode().clientCore.formPassword))
-			return Errors.makeErrorPage(this, "Buh! Invalid form password");
+		if(pass != null) {	/* FIXME: is this correct? what if the client just does not specify the password so that its null? */
+			if ((pass.length() == 0) || !pass.equals(pr.getNode().clientCore.formPassword))
+				return Errors.makeErrorPage(this, "Buh! Invalid form password");
+		}
 
 		String page = request.getPath().substring(PLUGIN_URI.length());
 		if ((page.length() < 1) || ("/".equals(page)))
