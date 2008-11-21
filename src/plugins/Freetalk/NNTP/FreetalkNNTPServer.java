da8ce19a028857bd2928870108c6344aa8b27f51
xor <xor@freenetproject.org>
2008-11-21 13:38:00 +0000

    Add some information on how to implement the NNTP server.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/Freetalk@23783 67a373e5-eb02-0410-a15c-ee090a768436

--- src/plugins/Freetalk/NNTP/FreetalkNNTPServer.java
+++ src/plugins/Freetalk/NNTP/FreetalkNNTPServer.java
@@ -3,6 +3,27 @@
  * http://www.gnu.org/ for further details of the GPL. */
 package plugins.Freetalk.NNTP;
 
+import plugins.Freetalk.FTIdentityManager;
+import plugins.Freetalk.FTMessageManager;
+import plugins.Freetalk.Freetalk;
+
+
+/**
+ * How to implement this:
+ * - Getting a board list: mMessageManager.boardIterator()
+ * - Getting messages in a board: mMessageManager.threadIterator()
+ * - Getting replies to a message: message.childrenIterator()
+ * - Things which might be missing: Functions in FTMessage and FTidentity for getting UIDs which are compatible to NNTP, plus functions
+ * in FTMessageManager/FTIdentityManager for retrieving by those UIDs. Ask for them and they will be implemented.
+ */
 public class FreetalkNNTPServer {
 
+	private FTIdentityManager mIdentityManager;
+	
+	private FTMessageManager mMessageManager;
+
+	public FreetalkNNTPServer(Freetalk ft) {
+		mIdentityManager = ft.getIdentityManager();
+		mMessageManager = ft.getMessageManager();
+	}
 }
