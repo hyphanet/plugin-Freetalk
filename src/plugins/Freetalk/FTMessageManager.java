f81cc13e94e4790e549891c1f0ce7833b66f3948
xor <xor@freenetproject.org>
2008-11-21 12:40:10 +0000

    Do the refactoring which was suggested by Bombe: Instead of passing the database as parameter to functions which need it, we store it in a transient member variable.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/Freetalk@23776 67a373e5-eb02-0410-a15c-ee090a768436

--- src/plugins/Freetalk/FTMessageManager.java
+++ src/plugins/Freetalk/FTMessageManager.java
@@ -18,6 +18,8 @@ import freenet.support.Logger;
  *
  */
 public abstract class FTMessageManager implements Runnable {
+	
+	protected final FTMessageManager self = this;
 
 	protected ObjectContainer db;
 	
@@ -36,6 +38,9 @@ public abstract class FTMessageManager implements Runnable {
 		mExecutor.execute(this, "FT Identity Manager");
 	}
 
+	/**
+	 * Get a message by its URI. The transient fields of the returned message will be initialized already.
+	 */
 	public synchronized FTMessage get(FreenetURI uri) {
 		Query query = db.query();
 		query.constrain(FTMessage.class);
@@ -43,10 +48,19 @@ public abstract class FTMessageManager implements Runnable {
 		ObjectSet<FTMessage> result = query.execute();
 
 		assert(result.size() <= 1);
-
-		return (result.size() == 0) ? null : result.next();
+		
+		if(result.size() == 0)
+			return null;
+		else {
+			FTMessage m = result.next();
+			m.initializeTransient(db);
+			return m;
+		}
 	}
 
+	/**
+	 * Get a board by its name. The transient fields of the returned board will be initialized already.
+	 */
 	public synchronized FTBoard getBoardByName(String name) {
 		Query query = db.query();
 		query.constrain(FTBoard.class);
@@ -55,21 +69,47 @@ public abstract class FTMessageManager implements Runnable {
 
 		assert(result.size() <= 1);
 
-		return (result.size() == 0) ? null : result.next();
+		if(result.size() == 0)
+			return null;
+		else {
+			FTBoard b = result.next();
+			b.initializeTransient(db, this);
+			return b;
+		}
 	}
 
 	/**
-	 * Get an iterator of all boards.
+	 * Get an iterator of all boards. The transient fields of the returned boards will be initialized already.
 	 */
 	public synchronized Iterator<FTBoard> boardIterator() {
-		/* FIXME: Accelerate this query. db4o should be configured to keep an alphabetic index of boards */
-		Query query = db.query();
-		query.constrain(FTBoard.class);
-		query.descend("mName").orderDescending();
+		return new Iterator<FTBoard>() {
+			private Iterator<FTBoard> iter;
+			
+			{
+				/* FIXME: Accelerate this query. db4o should be configured to keep an alphabetic index of boards */
+				Query query = db.query();
+				query.constrain(FTBoard.class);
+				query.descend("mName").orderDescending();
 
-		ObjectSet<FTBoard> result = query.execute();
+				ObjectSet<FTBoard> result = query.execute();
+				iter = result.iterator();
+			}
 
-		return result.iterator();
+			public boolean hasNext() {
+				return iter.hasNext();
+			}
+
+			public FTBoard next() {
+				FTBoard next = iter.next();
+				next.initializeTransient(db, self);
+				return next;
+			}
+
+			public void remove() {
+				throw new UnsupportedOperationException("Boards cannot be deleted yet.");
+			}
+			
+		};
 	}
 
 	/**
