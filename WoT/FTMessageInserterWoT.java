8797a780579ab2bcd9efa7fc0020bc557439725c
xor <xor@freenetproject.org>
2008-11-24 14:19:01 +0000

    More implementation.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/Freetalk@23841 67a373e5-eb02-0410-a15c-ee090a768436

--- WoT/FTMessageInserterWoT.java
+++ WoT/FTMessageInserterWoT.java
@@ -1,23 +1,31 @@
 package plugins.Freetalk.WoT;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Random;
 
 import freenet.client.FetchException;
 import freenet.client.FetchResult;
+import freenet.client.HighLevelSimpleClient;
 import freenet.client.InsertException;
 import freenet.client.async.BaseClientPutter;
 import freenet.client.async.ClientGetter;
 import freenet.keys.FreenetURI;
+import freenet.node.Node;
 import freenet.support.Executor;
+import plugins.Freetalk.FTIdentityManager;
 import plugins.Freetalk.FTMessage;
 import plugins.Freetalk.FTMessageInserter;
 import plugins.Freetalk.FTOwnIdentity;
 
 public class FTMessageInserterWoT extends FTMessageInserter {
 
-	public FTMessageInserterWoT(Executor myExecutor, String myName) {
-		super(myExecutor, myName);
-		// TODO Auto-generated constructor stub
+	private Random mRandom;
+	
+	public FTMessageInserterWoT(Node myNode, HighLevelSimpleClient myClient, String myName, FTIdentityManager myIdentityManager) {
+		super(myNode, myClient, myName, myIdentityManager);
+		mRandom = mNode.fastWeakRandom;
+		start();
 	}
 
 	@Override
@@ -27,75 +35,57 @@ public class FTMessageInserterWoT extends FTMessageInserter {
 	}
 
 	@Override
-	public Collection<ClientGetter> getFetchStorage() {
-		// TODO Auto-generated method stub
+	protected Collection<ClientGetter> createFetchStorage() {
 		return null;
 	}
 
 	@Override
-	public Collection<BaseClientPutter> getInsertStorage() {
-		// TODO Auto-generated method stub
-		return null;
+	protected Collection<BaseClientPutter> createInsertStorage() {
+		return new ArrayList<BaseClientPutter>(10);
 	}
 
 	@Override
-	public long getSleepTime() {
-		// TODO Auto-generated method stub
-		return 0;
+	protected long getStartupDelay() {
+		return STARTUP_DELAY/2 + mRandom.nextInt(STARTUP_DELAY);
+	}
+	
+	@Override
+	protected long getSleepTime() {
+		return THREAD_PERIOD/2 + mRandom.nextInt(THREAD_PERIOD);
 	}
 
 	@Override
-	public long getStartupDelay() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void iterate() {
+	protected void iterate() {
 		// TODO Auto-generated method stub
 
 	}
-
-	@Override
-	public void onFailure(FetchException e, ClientGetter state) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void onFailure(InsertException e, BaseClientPutter state) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void onFetchable(BaseClientPutter state) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void onGeneratedURI(FreenetURI uri, BaseClientPutter state) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void onMajorProgress() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void onSuccess(FetchResult result, ClientGetter state) {
-		// TODO Auto-generated method stub
-
-	}
-
+	
 	@Override
 	public void onSuccess(BaseClientPutter state) {
 		// TODO Auto-generated method stub
-
 	}
+	
+	@Override
+	public void onFailure(InsertException e, BaseClientPutter state) {
+		// TODO Auto-generated method stub
+	}
+
+	
+	/* Not needed functions*/
+	
+	@Override
+	public void onSuccess(FetchResult result, ClientGetter state) { }
+	
+	@Override
+	public void onFailure(FetchException e, ClientGetter state) { }
+	
+	@Override
+	public void onGeneratedURI(FreenetURI uri, BaseClientPutter state) { }
+	
+	@Override
+	public void onFetchable(BaseClientPutter state) { }
+
+	@Override
+	public void onMajorProgress() { }
 
 }
