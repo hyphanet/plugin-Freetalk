be0faa9b57c94e0e1c1d17229dd2c7b235dafd32
Benjamin Moody <benjamin.moody@gmail.com>
2008-11-26 03:20:40 +0000

    Added LIST and GROUP commands to the FreetalkNNTPHandler. Created the FreetalkNNTPGroup class for NNTP-related group functions. Javadoc added + various cleanups in handler + server classes.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/Freetalk@23869 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ src/plugins/Freetalk/ui/NNTP/FreetalkNNTPGroup.java
@@ -0,0 +1,59 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
+package plugins.Freetalk.ui.NNTP;
+
+import plugins.Freetalk.FTBoard;
+
+/**
+ * Object representing a newsgroup, as seen from the NNTP client's
+ * point of view.
+ *
+ * @author Benjamin Moody
+ */
+public class FreetalkNNTPGroup {
+	private final FTBoard board;
+
+	public FreetalkNNTPGroup(FTBoard board) {
+		this.board = board;
+	}
+
+	/**
+	 * Get the FTBoard object associated with this group.
+	 */
+	public FTBoard getBoard() {
+		return board;
+	}
+
+	/**
+	 * Estimate number of messages that have been posted.
+	 */
+	public long messageCount() {
+		return 0;				// FIXME
+	}
+
+	/**
+	 * Get the first valid message number.
+	 */
+	public long firstMessage() {
+		return 1;				// FIXME
+	}
+
+	/**
+	 * Get the last valid message number.
+	 */
+	public long lastMessage() {
+		return 0;				// FIXME
+	}
+
+	/**
+	 * Get the board posting status.  This is normally either "y"
+	 * (posting is allowed), "n" (posting is not allowed), or "m"
+	 * (group is moderated.)  It is a hint to the reader and doesn't
+	 * necessarily indicate whether the client will be allowed to
+	 * post, or whether any given message will be accepted.
+	 */
+	public String postingStatus() {
+		return "y";
+	}
+}
