ae889bea1f2747b8705de9a888f4b719fcbc8b32
xor <xor@freenetproject.org>
2008-12-02 00:30:54 +0000

    Add variables for storing dates of downloaded messagelists.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/Freetalk@23998 67a373e5-eb02-0410-a15c-ee090a768436

--- src/plugins/Freetalk/WoT/FTIdentityWoT.java
+++ src/plugins/Freetalk/WoT/FTIdentityWoT.java
@@ -3,6 +3,10 @@
  * http://www.gnu.org/ for further details of the GPL. */
 package plugins.Freetalk.WoT;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 import plugins.Freetalk.FTIdentity;
 
 import com.db4o.ObjectContainer;
@@ -19,8 +23,10 @@ public class FTIdentityWoT implements FTIdentity {
 	/* Attributes, stored in the database. */
 	
 	private final String mUID;
-    /** The requestURI used to fetch this identity from Freenet */
+    
+	/** The requestURI used to fetch this identity from Freenet */
 	private final FreenetURI mRequestURI;
+	
 	/** The nickname of this Identity */
 	private final String mNickname;
 	
@@ -35,10 +41,27 @@ public class FTIdentityWoT implements FTIdentity {
 	 */
 	private boolean mIsNeeded;
 	
+
+	/** The latest edition number of the identity's messagelist USK which we downloaded */
+	private int mLatestMessagelist;
+	
+	/** The date which the latest downloaded messagelist edition fully covers. I.e. if the messagelist contains only messages of date X then
+	 * this variable has to be set to X minus 1 because the next messagelist might still contain messages of date X */
+	private Date mLatestMessagelistDate;
+	
+	/** The oldest edition number of the identity's messagelist USK which we downloaded. */
+	private int mOldestMessagelist;
+	
+	/** The date which the oldest downloaded messagelist edition fully covers. I.e. if the messagelist contains only messages of date X then
+	 * this variable has to be set to X plus 1 because the previous messagelist might still contain messages of date X */
+	private Date mOldestMessagelistDate;
+	
 	
 	/* References to objects of the plugin, not stored in the database. */
 	
 	private transient ObjectContainer db;
+	
+	private transient static final Calendar mCalendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
 	
 
 	public FTIdentityWoT(String myUID, FreenetURI myRequestURI, String myNickname) {
@@ -47,6 +70,10 @@ public class FTIdentityWoT implements FTIdentity {
 		
 		mUID = myUID;
 		mRequestURI = myRequestURI;
+		mLatestMessagelist = -1;
+		mLatestMessagelistDate = null;
+		mOldestMessagelist = -1;
+		mOldestMessagelistDate = null;
 		mNickname = myNickname;
 		mLastReceivedFromWoT = System.currentTimeMillis();
 		mIsNeeded = false;
@@ -89,10 +116,37 @@ public class FTIdentityWoT implements FTIdentity {
 		mIsNeeded = newValue;
 		store();
 	}
+
+	public synchronized int getLatestMessagelist() {
+		return mLatestMessagelist;
+	}
+	
+	public synchronized void setLatestMessagelist(int newLatestMessagelist, Date newLatestMessagelistDate) {
+		mLatestMessagelist = newLatestMessagelist;
+		mLatestMessagelistDate = newLatestMessagelistDate;
+	}
+	
+	public synchronized Date getLatestMessagelistDate() {
+		return mLatestMessagelistDate;
+	}
+	
+	public synchronized int getOldestMessagelist() {
+		return mOldestMessagelist;
+	}
+	
+	public synchronized void setOldestMessagelist(int newOldestMessagelist, Date newOldestMessagelistDate) {
+		mLatestMessagelist = newOldestMessagelist;
+		mLatestMessagelistDate = newOldestMessagelistDate;
+	}
+	
+	public synchronized Date getOldestMessagelistDate() {
+		return mOldestMessagelistDate;
+	}
 	
 	public void store() {
 		/* FIXME: check for duplicates */
 		db.store(this);
 		db.commit();
 	}
+
 }
