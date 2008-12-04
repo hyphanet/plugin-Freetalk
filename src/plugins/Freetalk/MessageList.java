73c16a97b08e9a5fafeca8bfc238d3b4db3c3b37
xor <xor@freenetproject.org>
2008-12-04 21:34:45 +0000

    Add missing GPL.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/Freetalk@24052 67a373e5-eb02-0410-a15c-ee090a768436

--- src/plugins/Freetalk/MessageList.java
+++ src/plugins/Freetalk/MessageList.java
@@ -1,10 +1,11 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
 package plugins.Freetalk;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
-import com.db4o.ObjectContainer;
 
 import freenet.keys.FreenetURI;
 
