5f575a2d1d9a4fca8662396418a022cc693d3e4b
xor <xor@freenetproject.org>
2008-12-04 08:33:28 +0000

    Create classes.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/Freetalk@24032 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ WoT/FTMessageEncoderWoT.java
@@ -0,0 +1,16 @@
+package plugins.Freetalk.WoT;
+
+import plugins.Freetalk.FTMessage;
+
+/**
+ * Class to encode the message XML of Freetalk messages based on WoT.
+ * 
+ * @author xor
+ *
+ */
+public class FTMessageEncoderWoT {
+	
+	public String encodeMessage(FTMessage message) {
+		return null;
+	}
+}
---
+++ WoT/FTMessageParserWoT.java
@@ -0,0 +1,17 @@
+package plugins.Freetalk.WoT;
+
+import plugins.Freetalk.FTMessage;
+
+/**
+ * Class to decode the message XML of Freetalk messages based on WoT.
+ * 
+ * @author xor
+ *
+ */
+public class FTMessageParserWoT {
+	
+	public FTMessage parseMessage(String xml) {
+		return null;
+	}
+	
+}
