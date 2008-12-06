3ab8dbc52a91dbaf62192c1980b0b349852e8bb9
xor <xor@freenetproject.org>
2008-12-06 01:41:41 +0000

    GPL.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/Freetalk@24079 67a373e5-eb02-0410-a15c-ee090a768436

--- src/plugins/Freetalk/exceptions/InvalidParameterException.java
+++ src/plugins/Freetalk/exceptions/InvalidParameterException.java
@@ -1,8 +1,6 @@
-/**
- * This code is part of Freetalk, a plugin for Freenet. It is distributed 
- * under the GNU General Public License, version 2 (or at your option
- * any later version). See http://www.gnu.org/ for details of the GPL.
- */
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
 package plugins.Freetalk.exceptions;
 
 /* 
