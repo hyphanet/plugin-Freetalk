549d9bf8340108e08a42dcc35c2ea8afc6c2a7d2
xor <xor@freenetproject.org>
2008-12-09 22:43:39 +0000

    Move code upwards in the class hierarchy.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/Freetalk@24144 67a373e5-eb02-0410-a15c-ee090a768436

--- src/plugins/Freetalk/OwnMessage.java
+++ src/plugins/Freetalk/OwnMessage.java
@@ -3,7 +3,6 @@
  * http://www.gnu.org/ for further details of the GPL. */
 package plugins.Freetalk;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
@@ -13,26 +12,10 @@ import freenet.keys.FreenetURI;
 public final class OwnMessage extends Message {
 	
 	private boolean iWasInserted = false;
-	
-	private static final SimpleDateFormat mDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
 	public OwnMessage(Message newParentThread, Message newParentMessage, Set<Board> newBoards, Board newReplyToBoard, FTOwnIdentity newAuthor, String newTitle,
 			Date newDate, int newIndex, String newText, List<Attachment> newAttachments) {
 		super(generateRequestURI(newAuthor, newDate, newIndex), newParentThread.getURI(), newParentMessage.getURI(), newBoards, newReplyToBoard, newAuthor, newTitle, newDate, newIndex, newText, newAttachments);
-	}
-	
-	private static FreenetURI generateURI(FreenetURI baseURI, FTIdentity author, Date date, int index) {
-		String dayOfInsertion;
-		synchronized (mDateFormat) {
-			dayOfInsertion = mDateFormat.format(date);
-		}
-		baseURI = baseURI.setKeyType("SSK");
-		baseURI = baseURI.setDocName(Freetalk.PLUGIN_TITLE + "|" + "Message" + "|" + dayOfInsertion + "-" + index + ".xml");
-		return baseURI.setMetaString(null);
-	}
-	
-	private static FreenetURI generateRequestURI(FTOwnIdentity author, Date date, int index) {
-		return generateURI(author.getRequestURI(), author, date, index);
 	}
 
 	public FreenetURI getInsertURI() {
