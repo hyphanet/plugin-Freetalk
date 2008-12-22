5bbba7ecefe33867567259527784de7b48b466af
Benjamin Moody <benjamin.moody@gmail.com>
2008-12-22 08:33:26 +0000

    Support quoted-printable encoding in input messages. Also support the related "Q" quoting in headers. Base64 still needs to be done.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/Freetalk@24724 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ ui/NNTP/MIME/Base64Encoding.java
@@ -0,0 +1,31 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
+package plugins.Freetalk.ui.NNTP.MIME;
+
+import java.nio.ByteBuffer;
+
+/**
+ * The MIME Base64 encoding.  (See RFC 2045.)
+ */
+public class Base64Encoding extends TransferEncoding {
+
+	public Base64Encoding() {
+
+	}
+
+	public void setIgnoreErrors(boolean ignore) {
+		// FIXME: Implement
+		throw new UnsupportedOperationException();
+	}
+
+	public ByteBuffer encode(ByteBuffer input) {
+		// FIXME: Implement
+		throw new UnsupportedOperationException();
+	}
+
+	public ByteBuffer decode(ByteBuffer input) {
+		// FIXME: Implement
+		throw new UnsupportedOperationException();
+	}
+}
