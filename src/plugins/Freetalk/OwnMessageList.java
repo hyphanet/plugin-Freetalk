6a661947f0ba9a3764ec98a1876dc1b47b8d9f4d
xor <xor@freenetproject.org>
2009-01-09 17:41:00 +0000

    Start redesign of message insertion: Messages will be inserted as CHK instead of SSK. To obtain the CHK URIs, message lists will be used. The message lists will be inserted as SSK/USK. This was toad's idea, the reason is: 1. We need message lists to allow the plugin to be configured to only download messages from certain boards and not from all boards. 2. SSKs only offer 1 KiB of storage. Most messages will not fit into it, so the node will insert an additional CHK for the SSK. Because message lists will also be needed and be SSK, obtaining a message would result in 2 SSK lookups and 1 CHK lookup. If we insert the messages as CHK directly, we can save 1 SSK lookup.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/Freetalk@24989 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ src/plugins/Freetalk/OwnMessageList.java
@@ -0,0 +1,64 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
+package plugins.Freetalk;
+
+import java.util.ArrayList;
+
+import freenet.keys.FreenetURI;
+
+public final class OwnMessageList extends MessageList {
+	
+	private boolean iAmBeingInserted = false;
+	
+	private boolean iWasInserted = false;
+
+	private OwnMessageList(FTOwnIdentity newAuthor, int newIndex) {
+		super(newAuthor, newIndex, new ArrayList<FreenetURI>(32)); // TODO: Figure out a reasonable value
+	}
+	
+	public FreenetURI getInsertURI() {
+		return generateURI(((FTOwnIdentity)mAuthor).getInsertURI());
+	}
+	
+	/**
+	 * Add an <code>OwnMessage</code> to this <code>MessageList</code>.
+	 * This function synchronizes on the <code>MessageList</code> and the given message.
+	 */
+	public synchronized void addMessage(OwnMessage newMessage) {
+		synchronized(newMessage) {
+			if(iAmBeingInserted || iWasInserted)
+				throw new RuntimeException("Trying to add a message to a message list which is already being inserted.");
+			
+			if(newMessage.getAuthor() != mAuthor)
+				throw new IllegalArgumentException("Trying to add a message with wrong author " + newMessage.getAuthor() + " to an own message list of " + mAuthor);
+			
+			mMessages.add(newMessage.getRealURI());
+			newMessage.setMessageList(this);
+		}
+	}
+
+	/* Old code from OwnMessage, might be needed here maybe.
+	public synchronized void incrementInsertIndex() {
+		synchronized(OwnMessage.class) {
+			int freeIndex = mMessageManager.getFreeMessageIndex((FTOwnIdentity)mAuthor);
+			mIndex = Math.max(mIndex+1, freeIndex);
+			mID = generateID(mAuthor, mIndex);
+			store();
+		}
+	}
+	*/
+	
+	public synchronized void beginOfInsert() {
+		iAmBeingInserted = true;
+	}
+	
+	public synchronized boolean wasInserted() {
+		return iWasInserted;
+	}
+
+	public synchronized void markAsInserted() {
+		iWasInserted = true;
+	}
+
+}
