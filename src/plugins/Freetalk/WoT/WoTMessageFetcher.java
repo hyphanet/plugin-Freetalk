a315e6295ac9c393fd010599d618f95b809394ec
xor <xor@freenetproject.org>
2009-01-09 20:28:47 +0000

    Continue implementation of the new message fetching code.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/Freetalk@24994 67a373e5-eb02-0410-a15c-ee090a768436

--- src/plugins/Freetalk/WoT/WoTMessageFetcher.java
+++ src/plugins/Freetalk/WoT/WoTMessageFetcher.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.Random;
 import java.util.TimeZone;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -14,6 +15,7 @@ import java.util.concurrent.ArrayBlockingQueue;
 import plugins.Freetalk.FTIdentity;
 import plugins.Freetalk.Message;
 import plugins.Freetalk.MessageFetcher;
+import plugins.Freetalk.MessageList;
 import plugins.Freetalk.MessageXML;
 
 import com.db4o.ObjectContainer;
@@ -47,10 +49,7 @@ public class WoTMessageFetcher extends MessageFetcher {
 	
 	private final ObjectContainer db;
 	
-	/* FIXME FIXME FIXME: Use LRUQueue instead. ArrayBlockingQueue does not use a Hashset for contains()! */
-	private final ArrayBlockingQueue<FTIdentity> mIdentities = new ArrayBlockingQueue<FTIdentity>(PARALLEL_MESSAGE_FETCH_COUNT * 10); /* FIXME: figure out a decent size */
-	
-	private static final Calendar mCalendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+	private final Hashtable<ClientGetter, WoTMessageList> mMessageLists = new Hashtable<ClientGetter, WoTMessageList>(PARALLEL_MESSAGE_FETCH_COUNT * 2);
 
 	public WoTMessageFetcher(Node myNode, HighLevelSimpleClient myClient, ObjectContainer myDB, String myName, WoTIdentityManager myIdentityManager, WoTMessageManager myMessageManager) {
 		super(myNode, myClient, myName, myIdentityManager, myMessageManager);
@@ -89,93 +88,69 @@ public class WoTMessageFetcher extends MessageFetcher {
 	@Override
 	protected void iterate() {
 		Logger.debug(this, "Message fetcher loop running ...");
-		ArrayList<FTIdentity> identitiesToFetchFrom = new ArrayList<FTIdentity>(PARALLEL_MESSAGE_FETCH_COUNT);
-		
-		for(FTIdentity identity : mIdentityManager) {
-			synchronized(mIdentities) {
-				if(!mIdentities.contains(identity) && mIdentityManager.anyOwnIdentityWantsMessagesFrom(identity)) {
-					identitiesToFetchFrom.add(identity);
-					mIdentities.add(identity);
-				}
-			}
-		}
-		
-		/* mIdentities contains all identities which are available, so we have to flush it. */
-		if(identitiesToFetchFrom.size() == 0) {
-			 mIdentities.clear();
-			
-			 for(FTIdentity identity : mIdentityManager) {
-				 synchronized(mIdentities) {
-					 if(mIdentityManager.anyOwnIdentityWantsMessagesFrom(identity)) {
-						 identitiesToFetchFrom.add(identity);
-						 mIdentities.add(identity);
-					 }
-				 }
-			 }
-		}
-		
-		for(FTIdentity identity : identitiesToFetchFrom) {
-			try {
-				fetchMessages(identity);
-			}
-			catch(FetchException e) {
-				Logger.error(this, "Fetching of messages from " + identity.getNickname() + " failed.", e);
-			}
-		}
+
 		Logger.debug(this, "Message fetcher loop finished.");
 	}
 	
-	private void fetchMessages(FTIdentity author) throws FetchException {
-		fetchMessage(author, mMessageManager.getUnavailableMessageIndex(author));
+	private void fetchMessages(WoTMessageList list) {
+		for(MessageList.MessageReference ref : list) {
+			if(!ref.wasDownloaded()) {
+				try {
+					fetchMessage(list, ref.getURI());
+				}
+				catch(Exception e) {
+					Logger.error(this, "Error while trying to fetch message " + ref.getURI(), e);
+				}
+			}
+		}
 	}
-	
-	private void fetchMessage(FTIdentity author, int index) throws FetchException {
-		FreenetURI uri = Message.generateRequestURI(author, index);
+
+	private void fetchMessage(WoTMessageList list, FreenetURI uri) throws FetchException {
 		FetchContext fetchContext = mClient.getFetchContext();
 		fetchContext.maxSplitfileBlockRetries = 2;
 		fetchContext.maxNonSplitfileRetries = 2;
 		ClientGetter g = mClient.fetch(uri, -1, this, this, fetchContext);
 		//g.setPriorityClass(RequestStarter.UPDATE_PRIORITY_CLASS); /* pluginmanager defaults to interactive priority */
 		addFetch(g);
-		synchronized(mIdentities) {
-			if(!mIdentities.contains(author)) {
-				mIdentities.poll();	/* the oldest identity falls out of the FIFO and therefore message downloads from that one are allowed again */
-				try {
-					mIdentities.put(author); /* put this identity at the beginning of the FIFO */
-				} catch(InterruptedException e) {}
-			}
-		}
-		Logger.debug(this, "Trying to fetch message from " + uri.toString());
+		mMessageLists.put(g, list);
+		Logger.debug(this, "Trying to fetch message from " + uri);
 	}
 
 	@Override
-	public void onSuccess(FetchResult result, ClientGetter state) {
+	public synchronized void onSuccess(FetchResult result, ClientGetter state) {
 		Logger.debug(this, "Fetched message: " + state.getURI());
 		
 		try {
-			Message message = MessageXML.decode(mMessageManager, result.asBucket().getInputStream(), mIdentityManager.getIdentityByURI(state.getURI()), state.getURI());
+			Message message = MessageXML.decode(mMessageManager, result.asBucket().getInputStream(), mMessageLists.get(state), state.getURI());
 			mMessageManager.onMessageReceived(message);
-			removeFetch(state);
 		}
 		catch (Exception e) {
 			Logger.error(this, "Parsing failed for message " + state.getURI(), e);
+			/* FIXME: Mark non-parseable messages (in the MessageList) so that they do not block the download queue */ 
 		}
-		
-		try {
-			fetchMessage(mIdentityManager.getIdentityByURI(state.getURI()), Message.getIndexFromURI(state.getURI()) + 1);
-		} catch(Exception e) {
-			Logger.error(this, "Fetching of next message failed.", e);
+		finally {
+			removeFetch(state); /* FIXME: This was in the try{} block somewhere else in the FT/WoT code. Move it to finally{} there, too */
 		}
 	}
 	
 	@Override
-	public void onFailure(FetchException e, ClientGetter state) {
-		if(e.getMode() == FetchException.DATA_NOT_FOUND)
-			Logger.debug(this, "Downloading message " + state.getURI() + " failed, data not found.");
-		else
-			Logger.error(this, "Downloading message " + state.getURI() + " failed.", e);
+	public synchronized void onFailure(FetchException e, ClientGetter state) {
+		/* TODO: Handle DNF in some reasonable way. Mark the messages in the MessageList maybe.
+		if(e.getMode() == FetchException.DATA_NOT_FOUND) {
 		
+		}
+		*/
+		
+		Logger.error(this, "Downloading message " + state.getURI() + " failed.", e);
 		removeFetch(state);
+	}
+	
+	/**
+	 * You have to synchronize on this <code>WoTMessageFetcher</code> when using this function.
+	 */
+	protected void removeFetch(ClientGetter g) {
+		super.removeFetch(g);
+		mMessageLists.remove(g);
 	}
 	
 	
