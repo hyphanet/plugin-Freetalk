60053b95bf2fd4a2be2b53ecda60b9c0402ff1e3
xor <xor@freenetproject.org>
2009-02-02 06:10:44 +0000

    Bugfixes
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/Freetalk@25463 67a373e5-eb02-0410-a15c-ee090a768436

--- ui/web/NewThreadPage.java
+++ ui/web/NewThreadPage.java
@@ -12,7 +12,7 @@ public class NewThreadPage extends WebPageImpl {
 	
 	public NewThreadPage(WebInterface myWebInterface, FTOwnIdentity viewer, HTTPRequest request) throws NoSuchBoardException {
 		super(myWebInterface, viewer, request);
-		mBoard = mFreetalk.getMessageManager().getBoardByName(request.getParam("BoardName"));
+		mBoard = mFreetalk.getMessageManager().getBoardByName(request.getPartAsString("BoardName", 256)); /* FIXME: adapt to maximal board name length when it has been decided */
 	}
 
 	public void make() {
@@ -24,10 +24,12 @@ public class NewThreadPage extends WebPageImpl {
 		authorBox.addChild("b", mOwnIdentity.getFreetalkAddress());
 		
 		HTMLNode subjectBox = newThreadForm.addChild(getContentBox("Subject"));
-		subjectBox.addChild("input", new String[] {"type", "name", "size"}, new String[] {"text", "ThreadSubject", "150"}); /* FIXME: Find a reasonable maximal subject length and specify here and elsewhere */		
+		subjectBox.addChild("input", new String[] {"type", "name", "size"}, new String[] {"text", "ThreadSubject", "100"}); /* FIXME: Find a reasonable maximal subject length and specify here and elsewhere */		
 		
 		HTMLNode textBox = newThreadForm.addChild(getContentBox("Text"));
-		textBox.addChild("textarea", new String[] { "name", "cols", "rows" }, new String[] { "ThreadText", "150", "30" });
+		textBox.addChild("textarea", new String[] { "name", "cols", "rows" }, new String[] { "ThreadText", "80", "30" });
+		
+		newThreadForm.addChild("input", new String[] {"type", "name", "value"}, new String[] {"submit", "submit", "Submit"});
 	}
 
 }
