6e63dbddc494072b16c2387062cf27e7a36bcc81
xor <xor@freenetproject.org>
2009-02-03 22:41:01 +0000

    Logging.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/Freetalk@25527 67a373e5-eb02-0410-a15c-ee090a768436

--- src/plugins/Freetalk/exceptions/NoSuchMessageListException.java
+++ src/plugins/Freetalk/exceptions/NoSuchMessageListException.java
@@ -7,4 +7,8 @@ public class NoSuchMessageListException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 	
+	public NoSuchMessageListException(String messageListID) {
+		super(messageListID);
+	}
+	
 }
