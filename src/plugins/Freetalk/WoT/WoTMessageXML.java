29b3293f7d309477ddbe7805ded73556643f8841
xor <xor@freenetproject.org>
2009-02-25 05:57:46 +0000

    Organize imports.
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/Freetalk@25809 67a373e5-eb02-0410-a15c-ee090a768436

--- src/plugins/Freetalk/WoT/WoTMessageXML.java
+++ src/plugins/Freetalk/WoT/WoTMessageXML.java
@@ -4,7 +4,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
