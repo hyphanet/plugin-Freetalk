a1fd193db8c9d800bc15b187758dfe985abc2d41
bback <bbackde@googlemail.com>
2009-04-01 20:12:14 +0000

    Installed basic infrastructure for FCP interface
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/Freetalk@26321 67a373e5-eb02-0410-a15c-ee090a768436

--- src/plugins/Freetalk/ui/FCP/FCPInterface.java
+++ src/plugins/Freetalk/ui/FCP/FCPInterface.java
@@ -9,7 +9,22 @@ import freenet.pluginmanager.PluginReplySender;
 import freenet.support.SimpleFieldSet;
 import freenet.support.api.Bucket;
 
+/**
+ * FCP message format:
+ *   Command=name
+ *   ... 
+ */
 public final class FCPInterface implements FredPluginFCP {
+    
+    private final static int ERROR_EMPTY_MESSAGE        = 0;
+    private final static int ERROR_COMMAND_NAME_MISSING = 1;
+    private final static int ERROR_COMMAND_NAME_UNKNOWN = 2;
+    
+    private final static String[] ERROR_DESCRIPTIONS = {
+        "Got empty message",
+        "Invalid Command name",
+        "Command not implemented"
+    };
 
 	private final Freetalk mFreetalk;
 	
@@ -17,10 +32,55 @@ public final class FCPInterface implements FredPluginFCP {
 		mFreetalk = myFreetalk;
 	}
 
+    /**
+     * @param replysender interface to send a reply
+     * @param params parameters passed in, can be null
+     * @param data a bucket of data passed in, can be null
+     * @param access 0: direct call (plugin to plugin), 1: FCP restricted access,  2: FCP full access  
+     */
 	public void handle(PluginReplySender replysender, SimpleFieldSet params, Bucket data, int accesstype) {
-		SimpleFieldSet sfs = new SimpleFieldSet(true);
-		sfs.putOverwrite("Hello", "Nice try ;)");
-		sfs.putOverwrite("Sorry", "Not implemeted yet :(");
+	    if (params == null) {
+            sendError(replysender, ERROR_EMPTY_MESSAGE);
+            return;
+        }
+
+        final String command = params.get("Command");
+
+        if (command == null || command.trim().length() == 0) {
+            sendError(replysender, ERROR_COMMAND_NAME_MISSING);
+            return;
+        }
+
+        sendError(replysender, ERROR_COMMAND_NAME_UNKNOWN);
 	}
 
+   /**
+     * Send error to client.
+     * 
+     * @param replysender  the reply sender
+     * @param code         error code
+     */
+    private void sendError(PluginReplySender replysender, int code) {
+        sendError(replysender, code, ERROR_DESCRIPTIONS[code]);
+    }
+
+	/**
+	 * Send error to client.
+	 * 
+	 * Format:
+	 *   Status=Error
+	 *   Code=123
+	 *   Description=text...
+	 * 
+	 * @param replysender  the reply sender
+	 * @param code         error code
+	 * @param description  error description
+	 */
+	private void sendError(PluginReplySender replysender, int code, String description) {
+	    SimpleFieldSet sfs = new SimpleFieldSet(true);
+        sfs.putOverwrite("Status", "Error");
+        sfs.put("Code", code);
+        sfs.putOverwrite("Description", description);
+        replysender.send(sfs);
+    }
 }
