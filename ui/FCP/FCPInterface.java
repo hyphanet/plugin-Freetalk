777df8733512a333659cfce77e12dec0f09d11f8
bback <bbackde@googlemail.com>
2009-04-03 06:09:52 +0000

    switched to an easier infrastructure for now, and implemented a nice Ping command
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/plugins/Freetalk@26408 67a373e5-eb02-0410-a15c-ee090a768436

--- ui/FCP/FCPInterface.java
+++ ui/FCP/FCPInterface.java
@@ -18,16 +18,6 @@ import freenet.support.api.Bucket;
  */
 public final class FCPInterface implements FredPluginFCP {
     
-    private final static int ERROR_EMPTY_MESSAGE        = 0;
-    private final static int ERROR_COMMAND_NAME_MISSING = 1;
-    private final static int ERROR_COMMAND_NAME_UNKNOWN = 2;
-    
-    private final static String[] ERROR_DESCRIPTIONS = {
-        "Got empty message",
-        "Invalid Command name",
-        "Command not implemented"
-    };
-
 	private final Freetalk mFreetalk;
 	
 	public FCPInterface(Freetalk myFreetalk) {
@@ -41,52 +31,51 @@ public final class FCPInterface implements FredPluginFCP {
      * @param access 0: direct call (plugin to plugin), 1: FCP restricted access,  2: FCP full access  
      */
 	public void handle(PluginReplySender replysender, SimpleFieldSet params, Bucket data, int accesstype) {
-	    if (params == null) {
-            trySendError(replysender, ERROR_EMPTY_MESSAGE);
-            return;
+	    try {
+    	    if (params == null) {
+                throw new Exception("Empty message received");
+            }
+
+            String message = params.get("Message");
+            if (message == null || message.trim().length() == 0) {
+                throw new Exception("Specified message is empty");
+            }
+
+            if(message.equals("Ping")) {
+                replysender.send(handlePing(params), data);
+            } else {
+                throw new Exception("Unknown message (" + message + ")");
+            }
         }
-
-        final String command = params.get("Command");
-
-        if (command == null || command.trim().length() == 0) {
-            trySendError(replysender, ERROR_COMMAND_NAME_MISSING);
-            return;
+        catch (Exception e) {
+            Logger.error(this, e.toString());
+            try {
+                replysender.send(errorMessageFCP(params.get("Message"), e), data);
+            } catch (PluginNotFoundException e1) {
+                Logger.normal(this, "Connection to request sender lost", e1);
+            }
         }
-
-        trySendError(replysender, ERROR_COMMAND_NAME_UNKNOWN);
 	}
 
-   /**
-     * Send error to client.
-     * 
-     * @param replysender  the reply sender
-     * @param code         error code
-     */
-    private void trySendError(PluginReplySender replysender, int code) {
-        trySendError(replysender, code, ERROR_DESCRIPTIONS[code]);
+	/**
+	 * Simple Ping command handler. Returns a Pong.
+	 */
+    private SimpleFieldSet handlePing(SimpleFieldSet params) {
+        SimpleFieldSet sfs = new SimpleFieldSet(true);
+
+        sfs.putAppend("Message", "Pong");
+        sfs.put("utcMillis", System.currentTimeMillis());
+
+        return sfs;
     }
 
-	/**
-	 * Send error to client.
-	 * 
-	 * Format:
-	 *   Status=Error
-	 *   Code=123
-	 *   Description=text...
-	 * 
-	 * @param replysender  the reply sender
-	 * @param code         error code
-	 * @param description  error description
-	 */
-	private void trySendError(PluginReplySender replysender, int code, String description) {
-	    SimpleFieldSet sfs = new SimpleFieldSet(true);
-        sfs.putOverwrite("Status", "Error");
-        sfs.put("Code", code);
-        sfs.putOverwrite("Description", description);
-        try {
-			replysender.send(sfs);
-		} catch (PluginNotFoundException e) {
-			Logger.normal(this, "Connection to request sender lost");
-		}
+    private SimpleFieldSet errorMessageFCP(String originalMessage, Exception e) {
+        
+        SimpleFieldSet sfs = new SimpleFieldSet(true);
+        sfs.putAppend("Message", "Error");
+        sfs.putAppend("OriginalMessage", (originalMessage == null) ? "null" : originalMessage);
+        sfs.putAppend("Description", (e.getLocalizedMessage() == null) ? "null" : e.getLocalizedMessage());
+        e.printStackTrace();
+        return sfs;
     }
 }
