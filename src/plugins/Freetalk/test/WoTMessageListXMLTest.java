395b2c17bcbc28621570e51ebcb1f2abf6a2d8f9
xor <xor@freenetproject.org>
2009-07-13 13:31:38 +0200

    Modify the WoTMessageListXML test: It now adds messages to the message list using the message manager and garbage collects before encoding the list to XML. AND the test actually reproduces bug 3318: The messages *are* being added to the message list, but they do not appear in the final XML. So now I can debug it, nice.

--- src/plugins/Freetalk/test/WoTMessageListXMLTest.java
+++ src/plugins/Freetalk/test/WoTMessageListXMLTest.java
@@ -18,14 +18,24 @@ import plugins.Freetalk.WoT.WoTOwnIdentity;
 import plugins.Freetalk.WoT.WoTOwnMessage;
 import plugins.Freetalk.WoT.WoTOwnMessageList;
 import plugins.Freetalk.exceptions.NoSuchMessageException;
+import plugins.Freetalk.exceptions.NoSuchMessageListException;
 import freenet.keys.FreenetURI;
 import freenet.support.MultiValueTable;
 
 public class WoTMessageListXMLTest extends DatabaseBasedTest {
 	
 	private WoTMessageManager mMessageManager;
-	private WoTOwnMessage[] mMessages;
-	private WoTOwnMessageList mMessageList;
+	
+	private String mMessageListID;
+	
+		
+	/**
+	 * Stores the list of boards for each test message.
+	 * Key = Message ID
+	 * Value = Board name
+	 */
+	private MultiValueTable<String, String> mMessageBoards = new MultiValueTable<String, String>(16);
+	
 	
 	private String mHardcodedEncodedMessageList;
 
@@ -44,7 +54,7 @@ public class WoTMessageListXMLTest extends DatabaseBasedTest {
 		
 		mMessageManager = new WoTMessageManager(db, null);
 		
-		 mMessages = new WoTOwnMessage[] {
+		WoTOwnMessage[] messages = new WoTOwnMessage[] {
 			mMessageManager.postMessage(null, null, myBoards1, null, myAuthor, "title1", new Date(2009-1900, 06-1, 01), "text1", null),
 			mMessageManager.postMessage(null, null, myBoards2, null, myAuthor, "title2", new Date(2008-1900, 05-1, 02), "text2", null),
 			mMessageManager.postMessage(null, null, myBoards3, null, myAuthor, "title3", new Date(2007-1900, 04-1, 03),"text3", null),
@@ -56,29 +66,36 @@ public class WoTMessageListXMLTest extends DatabaseBasedTest {
 			new FreenetURI("CHK@H4nfdTqgQUQ0CkdPzvrs2F~IIkjOCnfEn~S042jUxuw,wkCrKtmvmYQzuo3f4v2JlB87wJkK0dspmGJ~ivztYP8,AAIA--8")
 		};
 
-		mMessageList = new WoTOwnMessageList(myAuthor, 123);
-		mMessageList.initializeTransient(db, mMessageManager);
+		WoTOwnMessageList messageList = new WoTOwnMessageList(myAuthor, 123);
+		messageList.initializeTransient(db, mMessageManager);
+		messageList.storeWithoutCommit();
+		db.commit();
 		
-		for(int i = 0; i < mMessages.length; ++i) {
-			mMessages[i].markAsInserted(messageURIs[i]);
-			mMessageList.addMessage(mMessages[i]);
+		mMessageListID = messageList.getID();
+		
+		for(int i = 0; i < messages.length; ++i) {
+			mMessageManager.onOwnMessageInserted(messages[i].getID(), messageURIs[i]);
 		}
 		
-		db.commit();
+		for(WoTOwnMessage message : messages) {
+			for(Board board : message.getBoards()) 
+				mMessageBoards.put(message.getID(), board.getName());
+		}
+	
 		
 		mHardcodedEncodedMessageList = new String(
 				"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>" + 
 				"<Freetalk-testing>" + 
 				"<MessageList Version=\"1\">" + 
-				"<Message Date=\"2009-06-01\" ID=\"" + mMessages[0].getID() + "\" URI=\"CHK@7qMS7LklYIhbZ88i0~u97lxrLKS2uxNwZWQOjPdXnJw,IlA~FSjWW2mPWlzWx7FgpZbBErYdLkqie1uSrcN~LbM,AAIA--8\">" + 
+				"<Message Date=\"2009-06-01\" ID=\"" + messages[0].getID() + "\" URI=\"CHK@7qMS7LklYIhbZ88i0~u97lxrLKS2uxNwZWQOjPdXnJw,IlA~FSjWW2mPWlzWx7FgpZbBErYdLkqie1uSrcN~LbM,AAIA--8\">" + 
 				"<Board Name=\"en.board1\"/>" + 
 				"<Board Name=\"en.board2\"/>" + 
 				"</Message>" + 
-				"<Message Date=\"2008-05-02\" ID=\"" + mMessages[1].getID() + "\" URI=\"CHK@0YUT4BEorqJCETQrLSgHBcw5RL7KQNm6Fbpo3ThzTy4,6RzUH23~TwPQ0IDQcgPoxEYX7yBTgTNydD~uJ0I9DTQ,AAIA--8\">" + 
+				"<Message Date=\"2008-05-02\" ID=\"" + messages[1].getID() + "\" URI=\"CHK@0YUT4BEorqJCETQrLSgHBcw5RL7KQNm6Fbpo3ThzTy4,6RzUH23~TwPQ0IDQcgPoxEYX7yBTgTNydD~uJ0I9DTQ,AAIA--8\">" + 
 				"<Board Name=\"en.board3\"/>" + 
 				"<Board Name=\"en.board4\"/>" + 
 				"</Message>" + 
-				"<Message Date=\"2007-04-03\" ID=\"" + mMessages[2].getID() + "\" URI=\"CHK@H4nfdTqgQUQ0CkdPzvrs2F~IIkjOCnfEn~S042jUxuw,wkCrKtmvmYQzuo3f4v2JlB87wJkK0dspmGJ~ivztYP8,AAIA--8\">" + 
+				"<Message Date=\"2007-04-03\" ID=\"" + messages[2].getID() + "\" URI=\"CHK@H4nfdTqgQUQ0CkdPzvrs2F~IIkjOCnfEn~S042jUxuw,wkCrKtmvmYQzuo3f4v2JlB87wJkK0dspmGJ~ivztYP8,AAIA--8\">" + 
 				"<Board Name=\"en.board5\"/>" + 
 				"<Board Name=\"en.board6\"/>" + 
 				"</Message>" + 
@@ -87,35 +104,38 @@ public class WoTMessageListXMLTest extends DatabaseBasedTest {
 				);
 	}
 
-	public void testEncode() throws TransformerException, ParserConfigurationException, NoSuchMessageException {
+	public void testEncode() throws TransformerException, ParserConfigurationException, NoSuchMessageException, NoSuchMessageListException {
 		ByteArrayOutputStream encodedMessageList = new ByteArrayOutputStream(4096);
-		WoTMessageListXML.encode(mMessageManager, mMessageList, encodedMessageList);
+		
+		System.gc(); db.purge(); System.gc();
+		
+		WoTMessageListXML.encode(mMessageManager, (WoTOwnMessageList)mMessageManager.getOwnMessageList(mMessageListID), encodedMessageList);
 		
 		assertEquals(mHardcodedEncodedMessageList, encodedMessageList.toString().replaceAll("[\r\n]", ""));
 	}
 
 	public void testDecode() throws Exception {
-		ByteArrayInputStream is = new ByteArrayInputStream(mHardcodedEncodedMessageList.getBytes());
-		WoTMessageList decodedList = WoTMessageListXML.decode(mMessageManager, mMessageList.getAuthor(), mMessageList.getURI(), is);
+		WoTMessageList decodedList;
 		
-		/* Store the list of boards for each test message. */
-		MultiValueTable<WoTOwnMessage, String> messageBoards = new MultiValueTable<WoTOwnMessage, String>(2 * mMessages.length);
-		for(WoTOwnMessage m : mMessages) {
-			for(Board b : m.getBoards()) 
-				messageBoards.put(m, b.getName());
+		{
+			ByteArrayInputStream is = new ByteArrayInputStream(mHardcodedEncodedMessageList.getBytes());
+			WoTOwnMessageList messageList = (WoTOwnMessageList)mMessageManager.getOwnMessageList(mMessageListID);
+			decodedList = WoTMessageListXML.decode(mMessageManager, messageList.getAuthor(), messageList.getURI(), is);
 		}
+
+		System.gc(); db.purge(); System.gc();
 		
 		/* Now we check every message reference we receive from the decoded XML. For each seen [message, board] pair we remove that
 		 * pair from the messageBoards table. If the table is empty at the end, the XML decoding has not dropped any of the pairs */
 		for(MessageList.MessageReference ref : decodedList) {
 			WoTOwnMessage message = (WoTOwnMessage)mMessageManager.getOwnMessage(ref.getMessageID());
 			assertTrue("A board was listed in the message list multiple times: " + ref.getBoard().getName(),
-					messageBoards.containsElement(message, ref.getBoard().getName()));
+					mMessageBoards.containsElement(message.getID(), ref.getBoard().getName()));
 			
-			messageBoards.removeElement(message, ref.getBoard().getName());
+			mMessageBoards.removeElement(message.getID(), ref.getBoard().getName());
 		}
 		
-		assertTrue("Not all boards or messages were specified in the message list.", messageBoards.isEmpty());
+		assertTrue("Not all boards or messages were specified in the message list.", mMessageBoards.isEmpty());
 	}
 
 }
