514abf83110d4fdc11242ec4386080e9df729a58
Matthew Toseland <toad@amphibian.dyndns.org>
2009-08-31 21:11:39 +0100

    L10n import fix

--- src/plugins/Freetalk/Freetalk.java
+++ src/plugins/Freetalk/Freetalk.java
@@ -24,7 +24,7 @@ import com.db4o.query.Query;
 import com.db4o.reflect.jdk.JdkReflector;
 
 import freenet.clients.http.PageMaker.THEME;
-import freenet.l10n.L10n.LANGUAGE;
+import freenet.l10n.BaseL10n.LANGUAGE;
 import freenet.pluginmanager.FredPlugin;
 import freenet.pluginmanager.FredPluginFCP;
 import freenet.pluginmanager.FredPluginL10n;
