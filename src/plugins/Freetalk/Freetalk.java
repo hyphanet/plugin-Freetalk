43e5da996068b8a97ab0b032a5fcb1bae2e0ed54
xor <xor@freenetproject.org>
2009-10-15 13:43:46 +0200

    Increment database format version and disable FAST_DEBUG_MODE.

--- src/plugins/Freetalk/Freetalk.java
+++ src/plugins/Freetalk/Freetalk.java
@@ -48,14 +48,14 @@ public class Freetalk implements FredPlugin, FredPluginFCP, FredPluginL10n, Fred
 	/* Constants */
 	
 	/** If set to true, all thread periods will be set to very low values, resulting in very fast message downloading. */
-	public static final boolean FAST_DEBUG_MODE = true; // FIXME: Set to false before release!
+	public static final boolean FAST_DEBUG_MODE = false; // FIXME: Set to false before release!
 	
 	public static final String PLUGIN_URI = "/Freetalk";
 	public static final String PLUGIN_TITLE = "Freetalk-testing"; /* FIXME REDFLAG: Has to be changed to Freetalk before release! Otherwise messages will disappear */
 	public static final String WOT_NAME = "plugins.WoT.WoT";
 	public static final String WOT_CONTEXT = "Freetalk";
-	public static final String DATABASE_FILENAME = "freetalk-testing-7.db4o";
-	public static final int DATABASE_FORMAT_VERSION = -93;
+	public static final String DATABASE_FILENAME = "freetalk-testing-8.db4o";
+	public static final int DATABASE_FORMAT_VERSION = -92;
 
 	/* References from the node */
 	
