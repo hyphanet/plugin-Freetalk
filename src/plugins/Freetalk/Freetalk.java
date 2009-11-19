f45ad339c36335694491e605fdd7feb2cebe5ff7
bback <bbackde@googlemail.com>
2009-11-19 20:34:09 +0100

    Start NNTP server if enabled in config.

--- src/plugins/Freetalk/Freetalk.java
+++ src/plugins/Freetalk/Freetalk.java
@@ -159,11 +159,12 @@ public class Freetalk implements FredPlugin, FredPluginFCP, FredPluginL10n, Fred
 
 		Logger.debug(this, "Creating FCP interface...");
 		mFCPInterface = new FCPInterface(this);
-		
-		// FIXME: Fix this: https://bugs.freenetproject.org/view.php?id=2977
-		//Logger.debug(this, "Starting NNTP server...");
-		mNNTPServer = new FreetalkNNTPServer(mPluginRespirator.getNode(), this, 1199, "127.0.0.1", "127.0.0.1");
-		//mNNTPServer = new FreetalkNNTPServer(mPluginRespirator.getNode(), this, 1199, "0.0.0.0", "*");
+
+		if (mConfig.getBoolean(Config.NNTP_SERVER_ENABLED)) {
+    		Logger.debug(this, "Starting NNTP server...");
+    		mNNTPServer = new FreetalkNNTPServer(mPluginRespirator.getNode(), this, 1199, "127.0.0.1", "127.0.0.1");
+    		//mNNTPServer = new FreetalkNNTPServer(mPluginRespirator.getNode(), this, 1199, "0.0.0.0", "*");
+		}
 		
 		Logger.debug(this, "Plugin loaded.");
 	}
