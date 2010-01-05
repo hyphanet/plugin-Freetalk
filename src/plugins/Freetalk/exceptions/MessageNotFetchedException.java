fa733d3ffac2ac64af48e9d926c3df8e7db5f4b3
xor <xor@freenetproject.org>
2010-01-05 16:59:24 +0100

    Refactoring: Introduce a new exception class: MessageNotFetchedException. This will be thrown by MessageReference.getMessage() if the associated message was not fetched yet. This happens when a thread reply was downloaded before the original thread was fetched. The old code usually returned null in those cases which does not fit to the general policy we have in Freetalk: Throw declared exceptions instead of returning null. The following commits will adopt all code to use the new exception instead of returning null.

---
+++ src/plugins/Freetalk/exceptions/MessageNotFetchedException.java
@@ -0,0 +1,18 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
+package plugins.Freetalk.exceptions;
+
+/**
+ * This exception is thrown when Freetalk has indication that a message with the given ID exists but was not able to fetch 
+ * the message yet. This usually happens when a reply of a thread is downloaded before the thread message itself.
+ */
+public class MessageNotFetchedException extends NoSuchMessageException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public MessageNotFetchedException(String id) {
+		super("The message with ID " + id + " was not fetched yet");
+	}
+
+}
