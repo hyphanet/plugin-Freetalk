hyphanet/plugin-Freetalk

commit 16e19f94876d510f38398d6d9467cc02abd64c67

Finished half-completed l10n support for SelectBoardsPage.

src/plugins/Freetalk/ui/web/SelectBoardsPage.java
@@ -45,19 +45,26 @@ public class SelectBoardsPage extends WebPageImpl {
 				if(subscribe) {
 					SubscribedBoard board = messageManager.subscribeToBoard(mOwnIdentity, boardName);
 
-					HTMLNode successBox = addContentBox("Subscription succeeded");
-					successBox.addChild("div", "You are now subscribed to the board ")
-						.addChild("u").addChild(
-							new HTMLNode("a", "href", 
-									Freetalk.PLUGIN_URI + "/showBoard?identity=" + mOwnIdentity.getID() + "&name=" + board.getName(), board.getName()));
+					HTMLNode successBox = addContentBox(Freetalk.getBaseL10n().getString("SelectBoardsPage.SubscriptionSucceededBoxHeader"));
+	                Freetalk.getBaseL10n().addL10nSubstitution(
+	                        successBox.addChild("div"), 
+	                        "SelectBoardsPage.SubscriptionSucceededBoxText",
+	                        new String[] { "link", "boardname", "/link" }, 
+	                        new String[] {
+	                                "<a href=\""+Freetalk.PLUGIN_URI+"/showBoard?identity=" + mOwnIdentity.getID() + "&name=" + board.getName()+"\">",
+	                                board.getName(),
+	                                "</a>" });
 				}
 				else if(unsubscribe) {
-					messageManager.unsubscribeFromBoard(mOwnIdentity, boardName);
-					addContentBox("Unsubscription succeeded")
-						.addChild("div", "You will no longer receive messages from the board " + boardName);
+                    HTMLNode successBox = addContentBox(Freetalk.getBaseL10n().getString("SelectBoardsPage.UnsubscriptionSucceededBoxHeader"));
+                    Freetalk.getBaseL10n().addL10nSubstitution(
+                        successBox.addChild("div"), 
+                        "SelectBoardsPage.UnsubscriptionSucceededBoxText",
+                        new String[] { "boardname" }, 
+                        new String[] { boardName });
 				}	
 			} catch(Exception e) {
-				HTMLNode alertBox = addAlertBox(subscribe ? "Subscribing to board failed" : "Unsubscribing from board failed");
+				HTMLNode alertBox = addAlertBox(subscribe ? Freetalk.getBaseL10n().getString("SelectBoardsPage.SubscribeFailed") : Freetalk.getBaseL10n().getString("SelectBoardsPage.UnsubscribeFailed"));
 				alertBox.addChild("div", e.getMessage());
 				
 				Logger.error(this, subscribe ? "subscribe failed" : "unsubscribe failed", e);

src/plugins/Freetalk/l10n/lang_en.l10n
@@ -42,6 +42,12 @@ SelectBoardsPage.BoardTableHeader.Subscribe=Subscribe
 SelectBoardsPage.BoardTableHeader.Unsubscribe=Unsubscribe
 SelectBoardsPage.BoardTable.SubscribeButton=Subscribe
 SelectBoardsPage.BoardTable.UnsubscribeButton=Unsubscribe
+SelectBoardsPage.SubscriptionSucceededBoxHeader=Subscription succeeded
+SelectBoardsPage.SubscriptionSucceededBoxText=You are now subscribed to the board ${link}${boardname}${/link}.
+SelectBoardsPage.UnsubscriptionSucceededBoxHeader=Unsubscription succeeded
+SelectBoardsPage.UnsubscriptionSucceededBoxText=You will no longer receive messages from the board ${boardname}.
+SelectBoardsPage.SubscribeFailed=Subscribing to board failed
+SelectBoardsPage.UnsubscribeFailed=Unsubscribing from board failed
 WebInterface.DiscussionMenuName=Discussion
 WebInterface.DiscussionMenuName.Tooltip=Message boards
 WebInterface.DiscussionMenuItem.Home=Home

commit 5792615bba465c8f4b40562e9a507a1f7e86a1a6

Commit 16e19f94876d510f38398d6d9467cc02abd64c67 broke the "unsubscribe" button. Fix that. Add FIXMEs.

src/plugins/Freetalk/ui/web/SelectBoardsPage.java
@@ -57,6 +57,8 @@ public class SelectBoardsPage extends WebPageImpl {
 	                                "</a>" });
 				}
 				else if(unsubscribe) {
+					messageManager.unsubscribeFromBoard(mOwnIdentity, boardName);
+					
                     HTMLNode successBox = addContentBox(l10n().getString("SelectBoardsPage.UnsubscriptionSucceededBox.Header"));
                     l10n().addL10nSubstitution(
                         successBox.addChild("div"), 