7a2300e2e281318c72cc11642edf773ec9ccd9b4
bback <bbackde@googlemail.com>
2010-01-14 20:51:31 +0100

    Resolved issue 0003800.

--- src/plugins/Freetalk/ui/web/BoardsPage.java
+++ src/plugins/Freetalk/ui/web/BoardsPage.java
@@ -9,6 +9,8 @@ import java.util.Iterator;
 import plugins.Freetalk.FTOwnIdentity;
 import plugins.Freetalk.Freetalk;
 import plugins.Freetalk.SubscribedBoard;
+import plugins.Freetalk.SubscribedBoard.BoardReplyLink;
+import plugins.Freetalk.SubscribedBoard.BoardThreadLink;
 import plugins.Freetalk.SubscribedBoard.MessageReference;
 import plugins.Freetalk.exceptions.NoSuchMessageException;
 import freenet.clients.http.RedirectException;
@@ -47,9 +49,10 @@ public final class BoardsPage extends WebPageImpl {
 		row.addChild("th", l10n().getString("BoardsPage.BoardTableHeader.Name"));
 		row.addChild("th", l10n().getString("BoardsPage.BoardTableHeader.Description"));
 		row.addChild("th", l10n().getString("BoardsPage.BoardTableHeader.Messages"));
+        row.addChild("th", l10n().getString("BoardsPage.BoardTableHeader.UnreadMessages"));
 		row.addChild("th", l10n().getString("BoardsPage.BoardTableHeader.LatestMessage"));
 		
-		DateFormat dateFormat = DateFormat.getInstance();
+		final DateFormat dateFormat = DateFormat.getInstance();
 		
 		int boardCount = 0;
 		
@@ -58,7 +61,7 @@ public final class BoardsPage extends WebPageImpl {
 			while(boards.hasNext()) {
 				++boardCount;
 				
-				SubscribedBoard board = boards.next();
+				final SubscribedBoard board = boards.next();
 				row = boardsTable.addChild("tr");
 
 				HTMLNode nameCell = row.addChild("th", new String[] { "align" }, new String[] { "left" });
@@ -71,18 +74,46 @@ public final class BoardsPage extends WebPageImpl {
 				/* Message count */
 				row.addChild("td", new String[] { "align" }, new String[] { "center" }, Integer.toString(board.messageCount()));
 				
-				/* Date of latest message */
-				String latestMessageDate;
-				boolean containsNewUnreadMessages = false;
-				try {
-					MessageReference latestMessage = board.getLatestMessage();
-					latestMessageDate = dateFormat.format(latestMessage.getMessageDate());
-					containsNewUnreadMessages = (latestMessage.wasRead() == false);
-				} catch(NoSuchMessageException e) {
-					latestMessageDate = "-";
-				}
-				
-				row.addChild(containsNewUnreadMessages ? "th" : "td", new String[] { "align" }, new String[] { "center" }, latestMessageDate);
+				/* Count unread messages + find latest message date */
+				// FIXME: Can this processing can become too slow? Maybe maintain values when new messages are inserted?
+				int unreadMessageCount = 0;
+				long latestMessageDate = 0;
+
+				for (BoardThreadLink boardThreadLink : board.getThreads()) {
+			        if (!boardThreadLink.wasThreadRead()) {
+			            // check top message (thread start message)
+			            if (!boardThreadLink.wasRead()) {
+			                unreadMessageCount++;
+			            }
+			            // check all replies
+		                for(BoardReplyLink reference : board.getAllThreadReplies(boardThreadLink.getThreadID(), true)) {
+		                    if (!reference.wasRead()) {
+                                unreadMessageCount++;
+		                    }
+		                }
+			        }
+			        // using maintained value from getLastReplyDate() instead of checking each single message date above
+			        long lastReplyMillis = boardThreadLink.getLastReplyDate().getTime();
+			        if (lastReplyMillis > latestMessageDate) {
+			            latestMessageDate = lastReplyMillis;
+			        }
+			    }
+
+			    final String latestMessageDateString;
+			    if (latestMessageDate == 0) {
+			        latestMessageDateString = "-";
+			    } else {
+			        latestMessageDateString = dateFormat.format(latestMessageDate);
+			    }
+
+			    // bold or not bold, thats the question here ...
+			    final String tableRowType = (unreadMessageCount > 0) ? "th" : "td";
+
+			    /* Unread messages count, bold when there are unread messages */
+	            row.addChild(tableRowType, new String[] { "align" }, new String[] { "center" }, Integer.toString(unreadMessageCount));
+
+			    /* Latest message date, bold when there are unread messages */
+				row.addChild(tableRowType, new String[] { "align" }, new String[] { "center" }, latestMessageDateString);
 			}
 		}
 
