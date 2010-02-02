hyphanet/plugin-Freetalk

commit 8a8ecc21dbf46526aa2d4fc0f3922c0769aba651

Resolved issue 0003833. BoardPage now shows unread message count per thread.

src/plugins/Freetalk/ui/web/BoardPage.java
@@ -74,6 +74,7 @@ public final class BoardPage extends WebPageImpl {
 			row.addChild("th", l10n().getString("BoardPage.ThreadTableHeader.Trust"));
 			row.addChild("th", l10n().getString("BoardPage.ThreadTableHeader.Date"));
 			row.addChild("th", l10n().getString("BoardPage.ThreadTableHeader.Replies"));
+			row.addChild("th", l10n().getString("BoardPage.ThreadTableHeader.Unread"));
 		
 		DateFormat dateFormat = DateFormat.getInstance();
 		
@@ -153,8 +154,27 @@ public final class BoardPage extends WebPageImpl {
 				/* Reply count */
 				row.addChild("td", new String[] { "align" }, new String[] { "center" }, 
 						Integer.toString(mBoard.threadReplyCount(threadReference.getThreadID())));
+				
+				/* Unread count */
+				int unreadCount = getUnreadCount(threadReference);
+				row.addChild((unreadCount>0) ? "th" : "td", new String[] { "align" }, new String[] { "center" }, Integer.toString(unreadCount));
 			}
 		}
+	}
+	
+	private int getUnreadCount(BoardThreadLink threadReference) {
+	    int unreadCount = 0;
+        if (!threadReference.wasRead()) {
+            unreadCount++;
+        }
+        
+        // scan all replies
+        for(BoardReplyLink reference : mBoard.getAllThreadReplies(threadReference.getThreadID(), true)) {
+            if(!reference.wasRead()) {
+                unreadCount++;
+            }
+        }
+        return unreadCount;
 	}
 
     /**

src/plugins/Freetalk/l10n/lang_en.l10n
@@ -5,6 +5,7 @@ BoardPage.ThreadTableHeader.Author=Author
 BoardPage.ThreadTableHeader.Trust=Trust
 BoardPage.ThreadTableHeader.Date=Date
 BoardPage.ThreadTableHeader.Replies=Replies
+BoardPage.ThreadTableHeader.Unread=Unread
 BoardPage.MarkAllThreadsAsReadButton=Mark all threads as read
 BoardsPage.BoardList.Header=Your boards
 BoardsPage.NewBoardButton=New board

commit e89140e5024b998061c7fdc37d13cfcebea43705

BoardPage: Fix inefficiency & other glitches introduced by commit 8a8ecc21dbf46526aa2d4fc0f3922c0769aba651

src/plugins/Freetalk/ui/web/BoardPage.java
@@ -141,7 +141,8 @@ public final class BoardPage extends WebPageImpl {
 
 				HTMLNode titleCell = row.addChild("td", new String[] { "align" }, new String[] { "left" });
 				
-				if(threadReference.wasThreadRead() == false)
+				final boolean threadWasRead = threadReference.wasThreadRead();
+				if(threadWasRead== false)
 					titleCell = titleCell.addChild("b");
 				
 				titleCell.addChild(new HTMLNode("a", "href", ThreadPage.getURI(mBoard, threadReference), threadTitle));
@@ -161,25 +162,10 @@ public final class BoardPage extends WebPageImpl {
 						Integer.toString(mBoard.threadReplyCount(threadReference.getThreadID())));
 				
 				/* Unread count */
-				int unreadCount = getUnreadCount(threadReference);
-				row.addChild((unreadCount>0) ? "th" : "td", new String[] { "align" }, new String[] { "center" }, Integer.toString(unreadCount));
+				int unreadCount = threadWasRead ? 0 : mBoard.threadUnreadReplyCount(threadReference.getThreadID());
+				row.addChild(threadWasRead ? "td" : "th", new String[] { "align" }, new String[] { "center" }, Integer.toString(unreadCount));
 			}
 		}
-	}
-	
-	private int getUnreadCount(BoardThreadLink threadReference) {
-	    int unreadCount = 0;
-        if (!threadReference.wasRead()) {
-            unreadCount++;
-        }
-        
-        // scan all replies
-        for(BoardReplyLink reference : mBoard.getAllThreadReplies(threadReference.getThreadID(), true)) {
-            if(!reference.wasRead()) {
-                unreadCount++;
-            }
-        }
-        return unreadCount;
 	}
 
     /**