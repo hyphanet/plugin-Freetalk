653c095a8fa368725293de86ed2994d897b7423a
xor <xor@freenetproject.org>
2010-02-10 15:28:57 +0100

    Continue implementation of Persistent.

--- src/plugins/Freetalk/Persistent.java
+++ src/plugins/Freetalk/Persistent.java
@@ -1,5 +1,7 @@
 package plugins.Freetalk;
 
+import java.util.Hashtable;
+
 import com.db4o.ext.ExtObjectContainer;
 
 import freenet.support.Logger;
@@ -13,25 +15,32 @@ import freenet.support.Logger;
  */
 public abstract class Persistent {
 	
-	protected transient IdentityManager mIdentityManager;
-	
-	protected transient MessageManager mMessageManager;
+	protected transient Freetalk mFreetalk;
 	
 	protected transient ExtObjectContainer mDB;
 	
+	protected transient static final Hashtable<Class<? extends Persistent>, String[]> mIndexedFields = new Hashtable<Class<? extends Persistent>, String[]>();
 
+	protected static final void registerIndexedFields(Class<? extends Persistent> clazz, String[] fields) {
+		mIndexedFields.put(clazz, fields);
+	}
 	/**
 	 * Has to be called after retrieving a persistent Object from the database to initialize its transient fields:<br />
 	 * Transient fields are NOT stored in the database. They are references to objects such as the IdentityManager.
 	 */
 	protected final void initializeTransient(Freetalk myFreetalk) {
-		mMessageManager = myFreetalk.getMessageManager();
-		mIdentityManager = mMessageManager.getIdentityManager();
-		mDB = myFreetalk.getDatabase();
+		mFreetalk = myFreetalk;
+		mDB = mFreetalk.getDatabase();
+	}
+	
+	protected final void activate(int depth) {
+		mDB.activate(this, depth);
 	}
 
 	/**
-	 * Used to check whether an object is active before storing it.<br /><br />
+	 * Only to be used by the extending classes, not to be called from the outside.
+	 * 
+	 * Used by storeWithoutCommit/deleteWithoutCommit to check whether an object is active before storing it.<br /><br />
 	 * 
 	 * Logs an error if the object is not active.<br /><br />
 	 * 
@@ -42,11 +51,43 @@ public abstract class Persistent {
 			if(!mDB.isActive(object))
 				Logger.error(this, "Trying to store a non-active object: " + object);
 				
-			mDB.activate(object, depth);
+			activate(depth);
 		}
 	}
 	
 	/**
+	 * Only to be used by the extending classes, not to be called from the outside.
+	 * 
+	 * Same as a call to {@link checkedActivate(this, depth)}
+	 */
+	protected final void checkedActivate(int depth) {
+		checkedActivate(this, depth);
+	}
+	
+	/**
+	 * Only to be used by the extending classes, not to be called from the outside.
+	 * 
+	 * Used by storeWithoutCommit for actually storing the object.<br /><br />
+	 * 
+	 * Currently does not any additional checks, it is used to 
+	 * @param object
+	 */
+	protected void checkedStore(Object object) {
+		mDB.store(object);
+	}
+	
+	/**
+	 * Only to be used by the extending classes, not to be called from the outside.
+	 * 
+	 * Same as a call to {@link checkedStore(this)}
+	 */
+	protected void checkedStore() {
+		mDB.store(this);
+	}
+	
+	/**
+	 * Only to be used by the extending classes, not to be called from the outside.
+	 * 
 	 * Checks whether an object is stored in the database and deletes it if it is.
 	 * If it was not found in the database, an error is logged.<br /><br />
 	 * 
@@ -59,8 +100,19 @@ public abstract class Persistent {
 			Logger.error(this, "Trying to delete a inexistent object: " + object);
 	}
 	
+	/**
+	 * Only to be used by the extending classes, not to be called from the outside.
+	 * 
+	 * Same as a call to {@link checkedDelete()}
+	 */
+	protected final void checkedDelete() {
+		checkedDelete(this);
+	}
+	
 	
 	/**
+	 * Only to be used by the extending classes, not to be called from the outside.
+	 * 
 	 * Checks whether the given object is stored in the database already and throws a RuntimeException if it is not.<br /><br />
 	 * 
 	 * This function is to be used as an integrity check in storeWithoutCommit() implementations which require that objects to which
@@ -78,30 +130,73 @@ public abstract class Persistent {
 		}
 	}
 
+	/**
+	 * Only to be used by the extending classes, not to be called from the outside.
+	 * 
+	 * Rolls back the current transaction, logs the passed exception and throws it.
+	 * To be used in try/catch blocks in storeWithoutCommit/deleteWithoutCommit.
+	 */
 	protected final void rollbackAndThrow(RuntimeException error) {
 		mDB.rollback(); Logger.error(this, "ROLLED BACK!", error);
 		throw error;
 	}
 	
-	protected void storeWithoutCommit() {
+
+	/**
+	 * Only to be used by the extending classes, not to be called from the outside.
+	 * 
+	 * When your extending class needs a different activation depth for store than 1, you have to override storeWithoutCommit() and make it call this function.
+	 * If you need to store other objects than this object (that is member objects) then you might want to copy the body of this function so that 
+	 * checkedActivate() is not called twice.
+	 * 
+	 * @param activationDepth The desired activation depth.
+	 */
+	protected void storeWithoutCommit(int activationDepth) {
 		try {		
 			// 1 is the maximal depth of all getter functions. You have to adjust this when introducing new member variables.
-			checkedActivate(this, 1);
-			mDB.store(this);
+			checkedActivate(activationDepth);
+			checkedStore(); // There is no checkedStore()
 		}
 		catch(RuntimeException e) {
 			rollbackAndThrow(e);
 		}
 	}
 	
-	protected void deleteWithoutCommit() {
+	/**
+	 * This is one of the only public functions which outside classes should use. It is used for storing the object.
+	 * The call to this function must be embedded in a transaction, that is a block of:<br />
+	 * synchronized(mDB.lock()) { try { object.storeWithoutCommit(); mDB.commit(); } catch(RuntimeException e) { Persistent.rollbackAndThrow(mDB, this, e); } } 
+	 */
+	protected void storeWithoutCommit() {
+		storeWithoutCommit(1);
+	}
+	
+	/**
+	 * Only to be used by the extending classes, not to be called from the outside.
+	 * 
+	 * When your extending class needs a different activation depth for store than 1, you have to override storeWithoutCommit() and make it call this function.
+	 * If you need to store other objects than this object (that is member objects) then you might want to copy the body of this function so that 
+	 * checkedActivate() is not called twice.
+	 * 
+	 * @param activationDepth The desired activation depth.
+	 */
+	protected void deleteWithoutCommit(int activationDepth) {
 		try {
 			// 1 is the maximal depth of all getter functions. You have to adjust this when introducing new member variables.
-			checkedActivate(this, 1);
+			checkedActivate(activationDepth);
 			checkedDelete(this);
 		}
 		catch(RuntimeException e) {
 			rollbackAndThrow(e);
 		}
+	}
+	
+	/**
+	 * This is one of the only public functions which outside classes should use. It is used for deleting the object.
+	 * The call to this function must be embedded in a transaction, that is a block of:<br />
+	 * synchronized(mDB.lock()) { try { object.deleteWithoutCommit(); mDB.commit(); } catch(RuntimeException e) { Persistent.rollbackAndThrow(mDB, this, e); } }
+	 */
+	protected void deleteWithoutCommit() {
+		deleteWithoutCommit(1);
 	}
 }
