bad7807af045037c431287ca653e116c44a264d0
xor <xor@freenetproject.org>
2010-12-07 04:16:39 +0100

    Class WoTOwnMessage: - Implement databaseIntegrityTest - Add missing GPL / @author

--- src/plugins/Freetalk/WoT/WoTOwnMessage.java
+++ src/plugins/Freetalk/WoT/WoTOwnMessage.java
@@ -1,3 +1,6 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
 package plugins.Freetalk.WoT;
 
 import java.util.Date;
@@ -9,8 +12,12 @@ import plugins.Freetalk.Message;
 import plugins.Freetalk.OwnIdentity;
 import plugins.Freetalk.OwnMessage;
 import plugins.Freetalk.exceptions.InvalidParameterException;
+import plugins.Freetalk.exceptions.NoSuchMessageException;
 import freenet.keys.FreenetURI;
 
+/**
+ * @author xor (xor@freenetproject.org)
+ */
 //@IndexedField // I can't think of any query which would need to get all WoTOwnMessage objects.
 public final class WoTOwnMessage extends OwnMessage {
 
@@ -27,6 +34,36 @@ public final class WoTOwnMessage extends OwnMessage {
 		super(null, null, MessageID.constructRandomID(newAuthor), null, myParentThreadURI,
 			  (newParentMessage == null ? null : newParentMessage.getURI()),
 			  newBoards, newReplyToBoard, newAuthor, newTitle, newDate, newText, newAttachments);
+	}
+	
+	@Override
+	public void databaseIntegrityTest() throws Exception {
+		super.databaseIntegrityTest();
+		
+		if(!(super.getURI() instanceof WoTMessageURI))
+			throw new IllegalStateException("super.getURI() == " + super.getURI());
+		
+		if(!(super.getAuthor() instanceof WoTOwnIdentity))
+			throw new IllegalStateException("super.getAuthor() == " + super.getAuthor());
+		
+		if(!(super.getThreadURI() instanceof WoTMessageURI))
+			throw new IllegalStateException("super.getThreadURI() == " + super.getThreadURI());
+		
+		if(!(super.getParentURI() instanceof WoTMessageURI))
+			throw new IllegalStateException("super.getParentURI() == " + super.getParentURI());
+		
+		try {
+			if(!(super.getThread() instanceof WoTMessage))
+				throw new IllegalStateException("super.getThread() == " + super.getThread());
+		} catch(NoSuchMessageException e) {}
+		
+		try {
+			if(!(super.getParent() instanceof WoTMessage))
+				throw new IllegalStateException("super.getParent() == " + super.getParent());
+		} catch(NoSuchMessageException e) {}
+		
+		if(!(super.getMessageList() instanceof WoTOwnMessageList))
+			throw new IllegalStateException("super.getMessageList() == " + super.getMessageList());
 	}
 
 	/**
