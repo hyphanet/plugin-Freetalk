7aa87779d382b2864688f9321ac687b46d76b2b3
xor <xor@freenetproject.org>
2010-12-07 02:47:06 +0100

    Class WoTMessage: Implement databaseIntegrityTest

--- src/plugins/Freetalk/WoT/WoTMessage.java
+++ src/plugins/Freetalk/WoT/WoTMessage.java
@@ -9,6 +9,7 @@ import plugins.Freetalk.Identity;
 import plugins.Freetalk.Message;
 import plugins.Freetalk.MessageList;
 import plugins.Freetalk.exceptions.InvalidParameterException;
+import plugins.Freetalk.exceptions.NoSuchMessageException;
 import freenet.keys.FreenetURI;
 
 /**
@@ -57,6 +58,36 @@ public final class WoTMessage extends Message {
 			String newText, List<Attachment> newAttachments) throws InvalidParameterException {
 		super(newURI, newFreenetURI, newID, newMessageList, newThreadURI, newParentURI, newBoards, newReplyToBoard, newAuthor, newTitle, newDate, newText,
 				newAttachments);
+	}
+	
+	@Override
+	public void databaseIntegrityTest() throws Exception {
+		super.databaseIntegrityTest();
+		
+		if(!(super.getURI() instanceof WoTMessageURI))
+			throw new IllegalStateException("super.getURI() == " + super.getURI());
+		
+		if(!(super.getAuthor() instanceof WoTIdentity))
+			throw new IllegalStateException("super.getAuthor() == " + super.getAuthor());
+		
+		if(!(super.getThreadURI() instanceof WoTMessageURI))
+			throw new IllegalStateException("super.getThreadURI() == " + super.getThreadURI());
+		
+		if(!(super.getParentURI() instanceof WoTMessageURI))
+			throw new IllegalStateException("super.getParentURI() == " + super.getParentURI());
+		
+		try {
+			if(!(super.getThread() instanceof WoTMessage))
+				throw new IllegalStateException("super.getThread() == " + super.getThread());
+		} catch(NoSuchMessageException e) {}
+		
+		try {
+			if(!(super.getParent() instanceof WoTMessage))
+				throw new IllegalStateException("super.getParent() == " + super.getParent());
+		} catch(NoSuchMessageException e) {}
+		
+		if(!(super.getMessageList() instanceof WoTMessageList))
+			throw new IllegalStateException("super.getMessageList() == " + super.getMessageList());
 	}
 
 	public static WoTMessageURI calculateURI(MessageList myMessageList, MessageID myID) {
