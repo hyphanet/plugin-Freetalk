0e7adf388465886cd4e9bd19ae85ae2923f5c678
Matthew Toseland <toad@amphibian.dyndns.org>
2011-06-23 21:16:43 +0100

    Fix for new client layer API.

--- src/plugins/Freetalk/WoT/WoTMessageInserter.java
+++ src/plugins/Freetalk/WoT/WoTMessageInserter.java
@@ -248,5 +248,12 @@ public final class WoTMessageInserter extends MessageInserter {
 
 	@Override
 	public void onMajorProgress(ObjectContainer container) { }
+
+	@Override
+	public void onGeneratedMetadata(Bucket metadata, BaseClientPutter state,
+			ObjectContainer container) {
+		metadata.free();
+		throw new UnsupportedOperationException();
+	}
 	
 }
