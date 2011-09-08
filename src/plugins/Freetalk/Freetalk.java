c9f86b37a0985fd5922950eac35f7f9d34128093
xor <xor@freenetproject.org>
2011-09-08 21:29:44 +0200

    Increment database format version. No upgrade code yet, I just want to make sure that old databases cannot be loaded.

--- src/plugins/Freetalk/Freetalk.java
+++ src/plugins/Freetalk/Freetalk.java
@@ -78,7 +78,7 @@ public final class Freetalk implements FredPlugin, FredPluginFCP, FredPluginL10n
 	public static final String WOT_PLUGIN_URI = "/WebOfTrust";
 	public static final String WOT_CONTEXT = PLUGIN_TITLE;
 	public static final String DATABASE_FILENAME = PLUGIN_TITLE + ".db4o";
-	public static final int DATABASE_FORMAT_VERSION = 1;
+	public static final int DATABASE_FORMAT_VERSION = 2;
 
 	/* References from the node */
 	
