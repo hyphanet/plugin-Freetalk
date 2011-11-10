86d196de936ba294dc13dd847b418fc4f7850a3d
Arne Babenhauserheide <bab@draketo.de>
2011-11-10 01:02:47 +0100

    db4o: cache the database access.
    Cherry-picked from ArneBab's commit
    c4b7d8068a29ce1aea0fb1b8b2e4c8fa4c74c883
    
    Conflicts:
    	src/plugins/Freetalk/Freetalk.java

--- src/plugins/Freetalk/Freetalk.java
+++ src/plugins/Freetalk/Freetalk.java
@@ -37,6 +37,10 @@ import com.db4o.Db4o;
 import com.db4o.ObjectContainer;
 import com.db4o.defragment.Defragment;
 import com.db4o.defragment.DefragmentConfig;
+
+import com.db4o.io.CachedIoAdapter;
+import com.db4o.io.RandomAccessFileAdapter;
+
 import com.db4o.ext.ExtObjectContainer;
 import com.db4o.query.Query;
 import com.db4o.reflect.jdk.JdkReflector;
@@ -276,6 +280,11 @@ public final class Freetalk implements FredPlugin, FredPluginFCP, FredPluginL10n
 	@SuppressWarnings("unchecked")
 	private com.db4o.config.Configuration getNewDatabaseConfiguration() {
 		final com.db4o.config.Configuration cfg = Db4o.newConfiguration();
+		
+		// use cached io
+		RandomAccessFileAdapter delegateAdapter = new RandomAccessFileAdapter();
+		// A cache with 4096 pages of 4096KB size, gives a 16MiB cache
+		cfg.io(new CachedIoAdapter(delegateAdapter,4096,4096));
 		
 		// Required config options:
 		
