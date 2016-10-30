ea5c15e6af6e0932d755f847fa21e7ffb9ef93bf
xor-freenet <xor@freenetproject.org>
2016-10-30 03:32:40 +0100

    DatabaseBasedTest: Add missing @Override annotations
    Automated using Eclipse, formatted by hand.

--- test/plugins/Freetalk/DatabaseBasedTest.java
+++ test/plugins/Freetalk/DatabaseBasedTest.java
@@ -40,7 +40,7 @@ public class DatabaseBasedTest extends TestCase {
 	/**
 	 * You have to call super.setUp() if you override this method.
 	 */
-	protected void setUp() throws Exception {
+	@Override protected void setUp() throws Exception {
 		super.setUp();
 		
 		File databaseFile = new File(getDatabaseFilename());
@@ -55,7 +55,7 @@ public class DatabaseBasedTest extends TestCase {
 	/**
 	 * You have to call super.tearDown() if you override this method. 
 	 */
-	protected void tearDown() throws Exception {
+	@Override protected void tearDown() throws Exception {
 		super.tearDown();
 		
 		db.close();
