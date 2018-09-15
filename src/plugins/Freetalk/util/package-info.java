594e05bac12f302cb8fe0029d9980da75a9ccb0c
xor-freenet <xor@freenetproject.org>
2018-09-15 16:43:19 +0200

    Add package plugins.Freetalk.util

---
+++ src/plugins/Freetalk/util/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Various generic utility classes which are not very specific to Freetalk.
+ * This could for example be general purpose data structures.
+ * 
+ * TODO: Code quality: Replace with usage of standard JRE classes or libraries. */
+package plugins.Freetalk.util;
