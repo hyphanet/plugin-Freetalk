236654a63f6608709843929d66fc61311ddf3c9a
xor-freenet <xor@freenetproject.org>
2019-10-19 01:42:40 +0200

    DatabaseBasedTest: Fix concurrent test execution by using random filename for database
    As opposed to the old Ant builder the new Gradle builder runs tests in
    parallel so this blew up because some tests have the same function name.

--- test/plugins/Freetalk/DatabaseBasedTest.java
+++ test/plugins/Freetalk/DatabaseBasedTest.java
@@ -3,6 +3,8 @@
  * http://www.gnu.org/ for further details of the GPL. */
 package plugins.Freetalk;
 
+import static java.lang.Math.abs;
+
 import java.io.File;
 
 import junit.framework.TestCase;
@@ -14,9 +16,9 @@ import freenet.crypt.DummyRandomSource;
 
 /**
  * A JUnit <code>TestCase</code> which opens a db4o database in setUp() and closes it in tearDown().
- * The filename of the database is chosen as the name of the test function currently run by db4o and the
- * file is deleted after the database has been closed. When setting up the test, it is assured that the database
- * file does not exist, the test will fail if it cannot be deleted.
+ * The filename of the database is chosen as the name of the test function currently run plus a
+ * random number, and the file is deleted after the database has been closed.
+ * When setting up the test, it is assured that the database file does not exist.
  * 
  * The database can be accessed through the member variable <code>db</code>.
  * 
@@ -27,6 +29,8 @@ import freenet.crypt.DummyRandomSource;
 public class DatabaseBasedTest extends TestCase {
 	
 	protected Freetalk mFreetalk;
+
+	private String mDatabaseFilename;
 
 	/**
 	 * The database used by this test.
@@ -40,7 +44,7 @@ public class DatabaseBasedTest extends TestCase {
 	 * @return Returns the filename of the database. This is the name of the current test function plus ".db4o".
 	 */
 	public String getDatabaseFilename() {
-		return getName() + ".db4o";
+		return mDatabaseFilename;
 	}
 
 	/**
@@ -52,6 +56,10 @@ public class DatabaseBasedTest extends TestCase {
 		Long seed = null; // Insert a seed here to re-run with it.
 		mRandom = new DummyRandomSource(seed != null ? seed : (seed = new Random().nextLong()));
 		System.out.println(this + " Random seed: " + seed);
+		
+		// Use a fresh Random to not obey the seed when generating the filename to ensure re-running
+		// with the same seed will not cause collision with the file of the last run.
+		mDatabaseFilename = getName() + abs(new Random().nextLong()) + ".db4o";
 		
 		File databaseFile = new File(getDatabaseFilename());
 		if(databaseFile.exists())
