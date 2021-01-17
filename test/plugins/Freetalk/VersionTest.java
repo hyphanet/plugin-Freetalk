690578b573f37c0ba6826bb940154c9d805337f4
xor-freenet <xor@freenetproject.org>
2021-01-17 22:48:41 +0100

    Add skeleton unit test VersionTest to test class Version

---
+++ test/plugins/Freetalk/VersionTest.java
@@ -0,0 +1,14 @@
+package plugins.Freetalk;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/** Tests class {@link Version}. */
+public final class VersionTest {
+
+	@Test public void testGetGitRevision() {
+		fail("FIXME: Implement");
+	}
+
+}
