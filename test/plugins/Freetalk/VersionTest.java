4389e90fce7df2083b266086f88b32043e347c61
xor-freenet <xor@freenetproject.org>
2021-01-19 00:18:40 +0100

    VersionTest: Documentation

--- test/plugins/Freetalk/VersionTest.java
+++ test/plugins/Freetalk/VersionTest.java
@@ -8,7 +8,12 @@ import org.junit.Test;
 public final class VersionTest {
 
 	@Test public void testGetGitRevision() {
+		// The legacy Ant builder ought to replace "@custom@" in "Version.java" with the revision.
+		// Thus that placeholder string must not be returned by getGitRevision().
 		assertNotEquals("@" + "custom" + "@", Version.getGitRevision());
+		
+		// The new Gradle builder instead must put the revision into the file "Version.properties".
+		// If getGitRevision() can't load the file it'll return "ERROR-while-loading-git-revision".
 		assertNotEquals("ERROR-while-loading-git-revision", Version.getGitRevision());
 	}
 
