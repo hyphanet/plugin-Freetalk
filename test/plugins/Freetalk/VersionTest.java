d87e4d92eb6f0902809dc6c33723779053e4cbb7
xor-freenet <xor@freenetproject.org>
2021-01-21 00:03:02 +0100

    VersionTest: Improve readability by testing more precisely

--- test/plugins/Freetalk/VersionTest.java
+++ test/plugins/Freetalk/VersionTest.java
@@ -9,8 +9,10 @@ public final class VersionTest {
 
 	@Test public void testGetGitRevision() {
 		// The legacy Ant builder ought to replace "@custom@" in "Version.java" with the revision.
-		// Thus that placeholder string must not be returned by getGitRevision().
-		assertNotEquals("@" + "custom" + "@", Version.getGitRevision());
+		// Test for accidental replacement beyond "Version.java":
+		assertEquals("@" + "custom" + "@", "@custom@");
+		// Test if the placeholder is actually replaced in the variable behind getGitRevision():
+		assertNotEquals("@custom@", Version.getGitRevision());
 		
 		// The new Gradle builder instead must put the revision into the file "Version.properties".
 		// If getGitRevision() can't load the file it'll return "ERROR-while-loading-git-revision".
