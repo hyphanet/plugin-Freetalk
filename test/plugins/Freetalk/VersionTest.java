32688f8d8fa50e161d02c9313d97311bd44db061
xor-freenet <xor@freenetproject.org>
2021-02-06 00:34:38 +0100

    VersionTest: Amend previous commit

--- test/plugins/Freetalk/VersionTest.java
+++ test/plugins/Freetalk/VersionTest.java
@@ -10,7 +10,7 @@ public final class VersionTest {
 	@Test public void testGetGitRevision() {
 		String gitRevision = Version.getGitRevision();
 		
-		assertTrue(gitRevision.length() > 0);
+		assertTrue(gitRevision.trim().length() > 0);
 		
 		// The legacy Ant builder ought to replace "@custom@" in "Version.java" with the revision.
 		// Test for accidental replacement beyond "Version.java":
